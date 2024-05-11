db17ae08c3278f57685e010bbba16c934b3f960c
alvenicum <alvenicum@gmail.com>
2024-05-11 16:37:13 +0200

    add lambda to control slot machine

--- pirate/src/main/java/com/mycompany/pirate/FonctionnalKernel/Controller/ControleSlotMachine.java
+++ pirate/src/main/java/com/mycompany/pirate/FonctionnalKernel/Controller/ControleSlotMachine.java
@@ -3,9 +3,11 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
 package com.mycompany.pirate.FonctionnalKernel.Controller;
+
 import com.mycompany.pirate.Boundary.interfaces.ISlotMachine;
 import java.util.Arrays;
 import java.util.Random;
+import java.util.stream.IntStream;
 
 /**
  *
@@ -14,16 +16,17 @@ import java.util.Random;
 public class ControleSlotMachine implements ISlotMachine {
     private final Random random = new Random();
     private int[] values = new int[3];
-    
 
     @Override
     public int[] spin() {
-        values[0] = random.nextInt(5); // Valeurs entre 0 et 4 pour la première case
-        values[1] = random.nextInt(4) + 1; // Valeurs entre 1 et 4 pour la deuxième case
-        values[2] = random.nextInt(4) + 1; // Valeurs entre 1 et 4 pour la troisième case
+        values = IntStream.generate(() -> random.nextInt(5)) // Valeurs entre 0 et 4
+                .limit(3)
+                .map(val -> val < 2 ? val : val + 1) // Assure que les deuxième et troisième cases ont des valeurs entre
+                                                     // 1 et 4
+                .toArray();
         return values;
     }
-    
+
     public int getSumValues() {
         return Arrays.stream(values).sum();
     }
