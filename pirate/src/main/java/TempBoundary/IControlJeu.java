1b65514b77eb3961b89739b8e1d66a33592dbee4
Matis Essengue <85627723+matis-essengue@users.noreply.github.com>
2024-05-15 12:10:07 +0200

    ajout scanner (mauvaise version)

---
+++ pirate/src/main/java/TempBoundary/IControlJeu.java
@@ -0,0 +1,5 @@
+package TempBoundary;
+
+public class IControlJeu {
+
+}
--- pirate/src/main/java/com/mycompany/pirate/FonctionnalKernel/Controller/ControlJeu.java
+++ pirate/src/main/java/com/mycompany/pirate/FonctionnalKernel/Controller/ControlJeu.java
@@ -12,7 +12,8 @@ public class ControlJeu implements IControlJeu {
       private Pion joueur2 = new Pion("Joueur 2");
       private Pion[] joueurs = { joueur1, joueur2 };
       private Plateau plateau = new Plateau(36);
-      
+      private Scanner scanner = new Scanner(System.in);
+
       // appelle des controlleurs
       private IDeplacerPion controlDeplacerPion = new ControlDeplacerPion();
       private ISlotMachine controleSlotMachine = new ControleSlotMachine();
@@ -22,15 +23,23 @@ public class ControlJeu implements IControlJeu {
             plateau.poserPion(joueur2);
       }
 
+      public void attendreTournerSlotMachine() {
+            System.out.println("Appuyez sur entrée pour tourner la slot machine");
+            scanner.nextLine();
+            int[] res = controleSlotMachine.spin();
+            System.out.println("Résultat du spin : " + res[0] + " " + res[1] + " " + res[2]);
+      }
+
       public void jouerTour(Pion joueurCourant) {
-            controleSlotMachine.spin();
+            attendreTournerSlotMachine();
             int deplacement = controleSlotMachine.getSumValues();
-            System.out.println(joueurCourant.getName() + " avance de " + deplacement + " cases et atterrit a la case " + (joueurCourant.getPosition() + deplacement));
+            System.out.println(joueurCourant.getName() + " avance de " + deplacement + " cases et atterrit a la case "
+                        + (joueurCourant.getPosition() + deplacement));
             int nouvellePosition = controlDeplacerPion.deplacerPion(joueurCourant, plateau, deplacement);
             System.out.println(joueurCourant.getName() + " est a la case " + nouvellePosition);
-            if(joueurCourant.getVie() <= 0){
-                System.out.println(joueurCourant.getName() + " n'a plus de vie");
-                jeuTermine = true;
+            if (joueurCourant.getVie() <= 0) {
+                  System.out.println(joueurCourant.getName() + " n'a plus de vie");
+                  jeuTermine = true;
             }
             if (nouvellePosition == plateau.getNbCases()) {
                   jeuTermine = true;
@@ -48,26 +57,26 @@ public class ControlJeu implements IControlJeu {
                               System.out.println("FIN DE PARTIE");
                               break;
                         } else {
-                            System.out.println("Fin du tour\n");
+                              System.out.println("Fin du tour\n");
                         }
                   }
-                  
+
             }
       }
-      
+
       @Override
-      public int deplacerPion(Pion pion, Plateau plateau, int deplacement){
-         return controlDeplacerPion.deplacerPion(pion, plateau, deplacement);
+      public int deplacerPion(Pion pion, Plateau plateau, int deplacement) {
+            return controlDeplacerPion.deplacerPion(pion, plateau, deplacement);
       }
-      
+
       @Override
-       public int[] spin() {
-          return controleSlotMachine.spin();
-       }
-       
+      public int[] spin() {
+            return controleSlotMachine.spin();
+      }
+
       @Override
-      public int getSumValues(){
-          return controleSlotMachine.getSumValues();
+      public int getSumValues() {
+            return controleSlotMachine.getSumValues();
       }
 
       public static void main(String[] args) {
@@ -75,12 +84,12 @@ public class ControlJeu implements IControlJeu {
             controlJeu.initialiserJeu();
             controlJeu.jouer();
       }
-      
+
       public Pion getJoueur1() {
-          return this.joueur1;
+            return this.joueur1;
       }
-      
+
       public Pion getJoueur2() {
-          return this.joueur2;
+            return this.joueur2;
       }
 }
